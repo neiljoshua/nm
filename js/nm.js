19bae64aacb6ac3a9d70683cc30021b71898168d
neiljoshua <neiljoshua@me.com>
2016-08-06 22:22:27 -0400

    updating images and correcting small moblie issues

--- js/nm.js
+++ js/nm.js
@@ -55,6 +55,15 @@ $('form').submit(function(e) {
     e.preventDefault();
 
     var $form  = $(this);
+    var $email = $("#email");
+    if ( !window.validateEmail( $email.val() ) &&  ( $email.prop('required') ) ) {
+      $email.parent().addClass('invalid');
+      $(".error").fadeTo(400, 1);
+
+    } else {
+
+    $email.parent().removeClass('invalid')
+    $(".error").fadeTo(400, 0);
 
      var url = "contact.php"; // the script where you handle the form input.
 
@@ -64,21 +73,78 @@ $('form').submit(function(e) {
          data: $("#contact-form").serialize(),
          success: function(data)
          {
-               $('form').html('<p>thank you!</p>');
+               $('.success').fadeTo(400, 1);
                $('form input').val('');
          }
        });
       return false;
+    }
 });
 
-function initialize() {
-    var mapProp = {
-      center:new google.maps.LatLng(40.774393,-73.92699099999999),
-      zoom:12,
-      mapTypeId:google.maps.MapTypeId.ROADMAP
-    };
-    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+// Form Validation
+window.validateEmail = function( emails ) {
+
+      var errors       = 0;
+      var emailArray   = (emails == null) ? [] :emails.split(',');
+      var expression   = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      $(emailArray).each(function(index, email){
+
+        if( !expression.test( email.trim() ) ){
+
+          errors++;
+
+        }
+
+      });
+
+      if( errors > 0 ){
+
+        return false;
+
+      }else{
+
+        return true;
+
+      }
+
+  }
+
+  $('.close-success').on('click', function(e) {
+    e.preventDefault();
+    $('.success').fadeTo(400, 0);
+    // body...
+  })
+
+  // Nav links on scroll
+
+  $(window).scroll(function () {
+    var e = $(window).scrollTop(),
+        t = $("header").height() - 50;
+        // console.log(e);
+        // console.log(t);
+    if (e >= t) {
+        // $("header").css({
+        //     "margin-top": 50
+        // });
+        console.log('passed header');
+        $("body > section").each(function (t) {
+          console.log(t);
+          console.log('hey');
+            if ($(this).position().top - 1 <= e - $(this).height() + 50) {
+              console.log('active');
+                $("#nav a.active").removeClass("active");
+                $("#nav a").eq(t).addClass("active")
+            }
+        })
+    } else {
+        $(".welcome").css({
+            "margin-top": 0
+        });
+        $("#nav a.active").removeClass("active");
+        $("#nav a:first").addClass("active")
+    }
+}).scroll();
+
 
 }); // End of doc ready.  
