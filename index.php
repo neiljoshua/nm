426d3b5188da68f8ae7c7ced730a29d48dcd0e06
neiljoshua <neiljoshua@me.com>
2016-11-25 21:21:27 -0500

    adding php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+	include('src/includes/header.php')
+?>
+
+	<section class="hero">
+				<h1> Neil J. Malpartida</h1>
+				<p> Web Developer </p>
+				<a href="#portfolio" class="explore"><i class="demo-icon icon-down-open"></i></a>
+	</section>
+
+<?php
+
+	include('src/includes/footer.php')
+
+?>
---
+++ src/includes/footer.php
@@ -0,0 +1,24 @@
+		    <footer>
+		    	<!-- <div id="googleMap" class="map"></div>--> 
+		    	<h2> &copy; Designed and developed by Neil Joshua  &mdash;  Email: <a href="mailto:neil@neiljoshua.com">neil@neiljoshua.com</a>
+		    	</h2>
+        		<div class="social">	
+                  <a href="http://twitter.com/NeilJMV"><i class="icon-twitter"></i></a>
+                  <a href="http://www.linkedin.com/in/neiljoshua"><i class="icon-linkedin-squared"></i></a>
+                  <a href="http://www.instagram.com/neiljoshua/"><i class="demo-icon icon-instagram"></i></a>
+                </div>  
+		    </footer>	
+	    </div>
+
+      <script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-82838385-1', 'auto');
+		  ga('send', 'pageview');
+	  </script>
+
+	</body>
+</html>
