55c85f8aea785c8f0a723ccedabefaf80ec2d9ba
neiljoshua <neiljoshua@me.com>
2016-12-19 06:11:00 -0500

    adding webkit to hero images

--- src/includes/header.php
+++ src/includes/header.php
@@ -4,6 +4,7 @@
 		<title>Neil Joshua Web Developer <?php if ($thisPage!="")echo " | $thisPage"; ?> </title>
 		<meta name="title" content="Neil Joshua <?php if ($thisPage!="") echo " | $thisPage"; ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, user-scalable=no" />
 		<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" type="text/css" href="src/css/styles.css">
 		<link rel="stylesheet" type="text/css" href="vendors.css">
