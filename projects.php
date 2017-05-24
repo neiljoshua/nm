a06089c5b92b11d9054d35f64959cac2f855eae8
neiljoshua <neiljoshua@me.com>
2017-05-23 20:56:31 -0400

    fixing links

--- projects.php
+++ projects.php
@@ -13,10 +13,10 @@
 	</div>
 	
 	<ul class="projects-list">
-		<li class="menu-item"><a href="../projects/sanisidro">San Isidro Group <i class="demo-icon icon-right"></i></a></li>
-		<li class="menu-item"><a href="../projectstarapoto">Tarapoto Resort <i class="demo-icon icon-right"></i></a> </li>
-		<li class="menu-item"><a href="../projectsbrandi">Brandi <i class="demo-icon icon-right"></i></a> </li>
-		<li class="menu-item"><a href="../projects/weatherapp">Weather App <i class="demo-icon icon-right"></i></a></li>
+		<li class="menu-item"><a href="sanisidro">San Isidro Group <i class="demo-icon icon-right"></i></a></li>
+		<li class="menu-item"><a href="tarapoto">Tarapoto Resort <i class="demo-icon icon-right"></i></a> </li>
+		<li class="menu-item"><a href="brandi">Brandi <i class="demo-icon icon-right"></i></a> </li>
+		<li class="menu-item"><a href="weatherapp">Weather App <i class="demo-icon icon-right"></i></a></li>
 	</ul>
 		
 	<p class="paragraph"> Some projects below may have been developed as prototypes or for demonstration purposes.</p>
