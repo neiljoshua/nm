3a4078e7e5b10fdb434a2a7938438c8f8f9a60fa
neiljoshua <neiljoshua@me.com>
2019-01-07 21:26:36 -0500

    Editing dev config

--- dev.config.js
+++ dev.config.js
@@ -5,13 +5,4 @@ module.exports = merge(common, {
   mode: 'development',
   watch: true,
   devtool: '#eval-source-map'
-  // devServer: {
-  //   // proxy: {
-  //   //   '**': {
-  //   //     context:['/index','/projects/index/'],
-  //   //     target: 'http://nm.local',
-  //   //     changeOrigin: true,
-  //   //   }
-  //   // }
-  // }
 });
