def19d7c6771d324c7aca3f743698a364ba9c4c8
neiljoshua <neiljoshua@me.com>
2019-07-06 08:07:39 -0400

    Removing Weather App

--- projects/index.php
+++ projects/index.php
@@ -23,9 +23,6 @@
         <a class="project-list__link" href="brandi">Brandi</a>
       </li>
       <li class="project-list__item">
-        <a class="project-list__link" href="weather">Weather App</a>
-      </li>
-      <li class="project-list__item">
         <a class="project-list__link" href="neiljoshua">Neil Joshua</a>
       </li>
     </ul>
